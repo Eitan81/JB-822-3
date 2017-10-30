8f5c26b87b9e0f016021317cec4349b917ca2ee2
Eitan81 <32844868+Eitan81@users.noreply.github.com>
2017-10-30 17:27:12 +0200

    Update Clock.java

--- src/ameedf/jb8223/assignments/hw02/Clock.java
+++ src/ameedf/jb8223/assignments/hw02/Clock.java
@@ -34,4 +34,104 @@ package ameedf.jb8223.assignments.hw02;
  * BONUS 2: the same functionality as in "BONUS 1" while using as few member variables / memory as possible
  */
 public class Clock {
+}package clock;
+
+
+/*
+ * 
+ * This class will represent a 24-hours clock up to micro-seconds precision (1000 millisecond = 1 second).
+ * That means that it will represent hours of the form "HH:MM:SS.mmm", where
+ * HH - represents the hours in 24 format. Range: [0-23]
+ * MM - represents the minutes. Range: [0-59]
+ * SS - represents the seconds. Range: [0-59]
+ * sss - represents the micro-seconds. Range: [0-999]
+ */
+
+public class Clock {
+	private int hours;
+	private int minutes;
+	private int seconds;
+	private int milliseconds;
+	
+	public int min = 0;
+	public int max = 1000000000;
+	
+/*
+ *   It will have 2 constructors:
+ * 		1. One constructor with 4 parameters: hours, minutes, seconds and milliseconds.
+ *    		Assume the input is always valid for this constructor
+ */
+	public Clock(int hours,int minutes, int seconds, int milliseconds) {
+		this.hours = hours;
+		this.minutes = minutes;
+		this.seconds = seconds;
+		this.milliseconds = milliseconds;
+	}
+	//	2. One constructor with no parameters. This will initialize the clock to 14:07:53.980
+	
+	public Clock() {
+		this.hours = 14;
+		this.minutes = 07;
+		this.seconds = 53;
+		this.milliseconds = 980;
+	}
+	/*
+	 *   1. addHours - with a single parameter which represents the number of hours to add.
+	 *    It will add the hours only if it's in the range [0..1,000,000,000]. No value is returned
+	 */
+	public void addHours(int hours) {
+		do {
+			this.hours = (hours+this.hours) % 24;
+			}
+			while ((hours <= max) || (hours >= min));
+	}
+		
+		/*
+		 *  2. addMinutes - with a single parameter which represents the number of minutes to add.
+		 *    It will add the minutes only if it's in the range [0..1,000,000,000]. No value is returned
+		 */
+		
+		
+		public void addMinutes(int minutes) {
+			do {
+				this.minutes = (minutes+this.minutes) % 60;
+				}
+				while ((minutes <= max) || (minutes >= min));	
+		}
+			 /* 3. addSeconds - with a single parameter which represents the number of seconds to add.
+			 *    It will add the seconds only if it's in the range [0..1,000,000,000]. No value is returned
+			*/
+			
+			public void addSeconds(int seconds) {
+				do {
+					this.seconds = (seconds+this.seconds) % 60;
+					}
+					while ((seconds <= max) || (seconds >= min));
+			}
+				/*
+				 * 4. addMilliseconds - with a single parameter which represents the number of milliseconds to add.
+				 *    It will add the milliseconds only if it's in the range [0..1,000,000,000]. No value is returned
+				 */
+				
+				
+				public void addMilliseconds(int milliseconds) {
+					do {
+						this.milliseconds = (milliseconds+this.milliseconds) % 1000;
+						}
+						while ((milliseconds <= max) || (milliseconds >= min));
+				}
+
+				
+				/*
+				 * 5. print - with a single boolean parameter that indicates weather we want to display a 24 hours clock
+				 *    or 12 hours clock. If a 12 hours clock is chosen it will add "AM/PM" at the end of the input.
+				 */
+				
+//				public static void print(Clock ) {
+	//				System.out.println(account1);
+		//		}
+
+
+	
 }
+
